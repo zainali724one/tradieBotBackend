b1323413f4f28cbd456475c6e6f86fa180dfcf9a
zain ali <zain.ali@724.one>
2025-06-02 18:02:29 +0500

    test

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,7 @@ exports.connectDb = async () => {
   try {
     await mongoose
       .connect(
-        "mongodb+srv://admin:obxHIrvo9DfU7Yix@cluster0.6el2dvk.mongodb.net/theHive?retryWrites=true&w=majority"
+        "mongodb+srv://kainatshakeel724:piYDhHj4fbc4UcfW@cluster0.gnyl3yr.mongodb.net/"
       )
       .then(() => {
         console.log("db is conncted");
