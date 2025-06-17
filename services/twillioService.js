6933ad31c6d977fb45d2e2b92f23c1343b674ccb
zain ali <zain.ali@724.one>
2025-06-17 18:44:19 +0500

    test

--- services/twillioService.js
+++ services/twillioService.js
@@ -6,11 +6,18 @@ const client = twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
  * @param {string} phoneNumber - Recipient's phone number (e.g., +12345678900)
  */
 const sendWhatsAppMessage = async (phoneNumber, messageBody) => {
-  return client.messages.create({
-    from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
-    to: `whatsapp:${phoneNumber}`,
-    body: messageBody,
-  });
+  return client.messages
+    .create({
+      from: `whatsapp:${process.env.TWILIO_WHATSAPP_NUMBER}`,
+      to: `whatsapp:${phoneNumber}`,
+      body: messageBody,
+    })
+    .then((res) => {
+      console.log(res, "res");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 module.exports = sendWhatsAppMessage;
