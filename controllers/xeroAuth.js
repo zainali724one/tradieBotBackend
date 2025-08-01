4043700d35d3713d7dfe54ab162af901fcafcf6c
Zain Ali <150250778+React724@users.noreply.github.com>
2025-08-01 21:49:00 +0500

    test

--- controllers/xeroAuth.js
+++ controllers/xeroAuth.js
@@ -3,7 +3,8 @@ const User = require("../models/User");
 const xero = require("../services/XeroClient");
 
 exports.getConsentUrl = catchAsyncError(async (req, res, next) => {
-  const { userId } = req.query;
+  // const { userId } = req.query;
+  const userId="6868056c5717c3a1bc283e1d"
   const url = await xero.buildConsentUrl();
   // res.send({ url });
 
