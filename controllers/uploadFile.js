6856bda01154ea7d075f32a01740708d6b375bf7
Zain Ali <150250778+React724@users.noreply.github.com>
2025-08-15 02:52:18 +0500

    test

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -1,5 +1,8 @@
-import fetch from 'node-fetch';
-import FormData from 'form-data';
+// import fetch from 'node-fetch';
+// import FormData from 'form-data';
+
+const FormData = require("form-data");
+const { default: fetch } = require("node-fetch");
 
  async function downloadAndUpload(req, res) {
   try {
