25fd34a1c772f565e6b9ba8675dfcd6250481ec7
sufyan724 <157563626+sufyan724@users.noreply.github.com>
2025-08-20 11:54:51 +0500

    make api of delete history on the basis of specific id

--- controllers/History.js
+++ controllers/History.js
@@ -2,6 +2,36 @@ const { catchAsyncError } = require("../middlewares/catchAsyncError");
 const History = require("../models/History");
 const { ErrorHandler } = require("../utils/ErrorHandler");
 
+
+
+
+exports.deleteHistory = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Validate Mongo ObjectId
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid history id" });
+    }
+
+    const deleted = await History.findByIdAndDelete(id);
+
+    if (!deleted) {
+      return res.status(404).json({ error: "History not found" });
+    }
+
+    return res.status(200).json({
+      message: "History deleted successfully",
+      id: deleted._id,
+    });
+  } catch (err) {
+    console.error("Delete history error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+
+
 exports.getHistory = catchAsyncError(async (req, res,next) => {
     const { telegramId } = req.params;
     if (!telegramId) {
